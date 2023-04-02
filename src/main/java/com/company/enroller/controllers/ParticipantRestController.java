7dab5dfec55670aef54ecd5b4755a3554249a5ae
vKapseLv <mpioro.edu@gmail.com>
2023-04-02 15:56:24 +0200

    Added CRUD.

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -5,9 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.ParticipantService;
@@ -24,5 +22,67 @@ public class ParticipantRestController {
 		Collection<Participant> participants = participantService.getAll();
 		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
 	}
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getParticipant(@PathVariable("id") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	}
 
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> registerParticipant(@RequestBody Participant participant) {
+		if(participant.getLogin() == null || participant.getPassword() == null) {
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+
+		if(participantService.findByLogin(participant.getLogin()) != null) {
+			return new ResponseEntity(HttpStatus.IM_USED);
+		}
+
+		if(!participantService.addParticipant(participant)){
+			return new ResponseEntity(HttpStatus.CONFLICT);
+		};
+
+		if(participantService.findByLogin(participant.getLogin()) != null) {
+			return new ResponseEntity(HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity(HttpStatus.CONFLICT);
+		}
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteParticipant(@RequestBody Participant participant) {
+		Participant foundParticipant = participantService.findByLogin(participant.getLogin());
+		if (foundParticipant == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+
+		if(!participantService.removeParticipant(foundParticipant)){
+			return new ResponseEntity(HttpStatus.CONFLICT);
+		};
+
+		if(participantService.findByLogin(participant.getLogin()) == null) {
+			return new ResponseEntity(HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity(HttpStatus.IM_USED);
+		}
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.PUT)
+	public ResponseEntity<?> updateParticipant(@RequestBody Participant participant) {
+		Participant foundParticipant = participantService.findByLogin(participant.getLogin());
+		if (foundParticipant == null) {
+			return new ResponseEntity("User "+participant.getLogin()+" not found.", HttpStatus.NOT_FOUND);
+		}
+
+		if(!participantService.updateParticipant(foundParticipant, participant)){
+			return new ResponseEntity("Unable to update "+participant.getLogin()+"'s password." , HttpStatus.CONFLICT);
+		};
+
+		return new ResponseEntity("Updated "+participant.getLogin()+"'s password." , HttpStatus.OK);
+	}
 }
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,10 +2,16 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @Component("participantService")
 public class ParticipantService {
@@ -22,4 +28,57 @@ public class ParticipantService {
 		return query.list();
 	}
 
+    public Participant findByLogin(String login) {
+		String hql = "FROM Participant p WHERE p.login = :login";
+		Query query = connector.getSession().createQuery(hql);
+		query.setParameter("login", login);
+		Collection<Participant> foundParticipants = query.list();
+
+		if(foundParticipants == null || foundParticipants.size() == 0) {
+			return null;
+		}
+		return (Participant) foundParticipants.toArray()[0];
+    }
+
+	public boolean addParticipant(Participant participant) {
+		try {
+			Transaction tr = connector.getSession().getTransaction();
+			tr.begin();
+			connector.getSession().save(participant);
+			tr.commit();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean removeParticipant(Participant participant) {
+		try {
+			Transaction tr = connector.getSession().getTransaction();
+			tr.begin();
+			connector.getSession().delete(participant);
+			tr.commit();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean updateParticipant(Participant foundParticipant, Participant participant) {
+		if(participant.getPassword() == null) {
+			return false;
+		}
+		try {
+			Transaction tr = connector.getSession().getTransaction();
+			tr.begin();
+			foundParticipant.setPassword(participant.getPassword());
+			connector.getSession().save(foundParticipant);
+			tr.commit();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
